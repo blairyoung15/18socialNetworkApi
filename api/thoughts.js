a4fd840b1cbd6f8f2b1ddfee8ee8a65551606323
blairyoung15 <bey99d@gmail.com>
2022-07-13 10:39:33 -0500

    updated users and thoughts api

--- api/thoughts.js
+++ api/thoughts.js
@@ -5,5 +5,49 @@
 // DELETE to remove a thought by its _id
 
 ///api/thoughts/:thoughtId/reactions
-    //POST to create a reaction stored in a single thought's reactions array field
-    //DELETE to pull and remove a reaction by the reaction's reactionId value
+//POST to create a reaction stored in a single thought's reactions array field
+//DELETE to pull and remove a reaction by the reaction's reactionId value
+
+const router = require('express').Router();
+
+const {
+    allThoughtsGet,
+    idThoughtsGet,
+    thoughtsCreate,
+    idThoughtsUpdate,
+    idThoughtsDelete,
+    reactionCreate,
+    reactionDelete
+
+} = require('../../controllers/thoughts-controller');
+
+// get to get all thoughts
+router
+    .route('/')
+    .get(allThoughtsGet);
+
+// get to get a single thought by its _id
+// PUT to update a thought by its _id
+// DELETE to remove a thought by its _id
+router
+    .route('/:id')
+    .get(idThoughtsGet)
+    .put(idThoughtsUpdate)
+    .delete(idThoughtsDelete);
+
+// POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+router
+    .route('/:userId')
+    .post(thoughtsCreate);
+
+//POST to create a reaction stored in a single thought's reactions array field
+router
+    .route('/:thoughtId/reactions')
+    .post(reactionCreate);
+
+//DELETE to pull and remove a reaction by the reaction's reactionId value
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(reactionDelete);
+
+module.exports = router;
--- api/users.js
+++ api/users.js
@@ -7,3 +7,41 @@
 // /api/users/:userId/friends/:friendId
     //POST to add a new friend to a user's friend list
     //DELETE to remove a friend from a user's friend list
+
+    const router = require('express').Router();
+
+    const {
+        allUsersGet,
+        idUsersGet,
+        usersCreate,
+        idUsersUpdate,
+        idUsersDelete,
+        friendAdd,
+        friendDelete
+    
+    } = require('../../controllers/users-controller');
+    
+    // get all users
+    // post a new user
+    router
+      .route('/')
+      .get(allUsersGet)
+      .post(usersCreate);
+    
+    // get a single user by its _id and populated thought and friend data
+    // put to update a user by its _id
+    // delete to remove user by its _id
+    router
+      .route('/:id')
+      .get(idUsersGet)
+      .put(idUsersUpdate)
+      .delete(idUsersDelete);
+
+    //POST to add a new friend to a user's friend list
+    //DELETE to remove a friend from a user's friend list
+    router 
+        .route('/:id/friends/:friendId')
+        .post(friendAdd)
+        .delete(friendDelete);
+    
+    module.exports = router;
