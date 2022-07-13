1ba507fab3bdcb484caafb5db3ac7d9fc5f20894
blairyoung15 <bey99d@gmail.com>
2022-07-13 10:07:33 -0500

    outlined api for thoughts and users

---
+++ api/users.js
@@ -0,0 +1,9 @@
+// get all users
+// get a single user by its _id and populated thought and friend data
+// post a new user
+// put to update a user by its _id
+// delete to remove user by its _id
+
+// /api/users/:userId/friends/:friendId
+    //POST to add a new friend to a user's friend list
+    //DELETE to remove a friend from a user's friend list
